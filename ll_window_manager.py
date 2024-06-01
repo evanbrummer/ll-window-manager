b723d502e9464f1a8bc8603cd6e12d9653f53c26
Evan <77951826+evanbrummer@users.noreply.github.com>
2024-05-31 23:02:10 -0500

    Remove old stuff

--- ll_window_manager.py
+++ ll_window_manager.py
@@ -1,19 +1,5 @@
 from tkinter import *
  
-# root = Tk()
-# root.title("winmgr")
- 
-# C = Canvas(root, bg="yellow", height=250, width=300) 
-
-# line = C.create_line(108, 120, 320, 40, fill="green") 
-
-# arc = C.create_arc(180, 150, 80, 210, start=0, extent=220, fill="red") 
-
-# oval = C.create_oval(80, 30, 140, 150, fill="blue")
- 
-# C.pack()
-# mainloop()
-
 def on_start(event):
   global rectangle, dragging, start_x, start_y
   dragging = True
